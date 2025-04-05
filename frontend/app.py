f2039c59c107c3718c28daa9a8b131fd52989f7d
Andrey Vinajera Zamora <avinajera2022@gmail.com>
2025-04-05 08:14:17 -0400

    Initial commit

---
+++ frontend/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+def main():
+    """
+    Main function to run the Versat Virtual Assistant application.
+    """
+    # Configure Streamlit page
+    st.set_page_config(
+        page_title="Asistente Virtual Versat",
+        page_icon=" 📚",
+        layout="wide",
+    )
+
+    # Title and description
+    st.title("📚 Asistente Virtual Versat!!!---")
+    st.markdown("Bienvenido al Asistente Virtual Versat.")
+    
+    
+    
+    
+    
+
+
+if __name__ == "__main__":
+    main()
